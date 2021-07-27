79f3e3db6f0c26ac559b5d61bc194fb957f44605
Minseok-Choi <cms05041@gmail.com>
2021-07-27 17:51:15 +0900

    +bruteForce

---
+++ algorithm-BOJ-step/src/BOJ_bruteForce/BOJ2231.java
@@ -0,0 +1,41 @@
+package BOJ_bruteForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2231 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String num = br.readLine();
+		
+		int numLength =num.length(); // 자릿수
+		
+		int N = Integer.parseInt(num);
+		
+		int constructor = 0;
+		
+		// N보다 작은 수에 대해 모두 조회하기 
+		// for (int i = 0; i < N; i++) {
+		
+		// 생성자가 나올 수 있는 최솟값부터 조회하기 
+		for (int i = N-(numLength*9); i < N; i++) { 
+			int number = i;
+			int sum = 0;
+			
+			while(number != 0) { // 자릿수 더하기 
+				sum += number % 10;
+				number /= 10;
+			}
+			
+			if(sum + i == N) { // 생성자를 찾았을 때 
+				constructor = i;
+				break;
+			}
+		}
+		System.out.println(constructor);
+	}
+	
+}
---
+++ algorithm-BOJ-step/src/BOJ_bruteForce/BOJ2798.java
@@ -0,0 +1,49 @@
+package BOJ_bruteForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2798 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] card = new int[N];
+		
+		st = new StringTokenizer(br.readLine()," ");
+		for (int i = 0; i < N; i++) {
+			card[i] =Integer.parseInt(st.nextToken());			
+		}
+		System.out.println(blackJack(card, N, M));
+		
+	}
+	
+	public static int blackJack(int[] card, int number, int sum) {
+		int maxSum = 0;
+		for (int i = 0; i < number - 2 ; i++) {
+			if(card[i] > sum) continue;
+			
+			for (int j = i+1; j < number -1; j++) {
+				if(card[i]+card[j] > sum) continue;
+				
+				for (int k = j+1; k < number; k++) {
+					int temp = card[i]+card[j]+card[k];
+					
+					if(sum == temp) return temp;
+					
+					if(maxSum < temp && temp < sum) {
+						maxSum = temp;
+					}
+				}
+			}
+		}
+		
+		return maxSum;
+	}
+}
