7ca8d82873278c3458b9a553278e42437286f1f9
Minseok-Choi <cms05041@gmail.com>
2021-07-30 21:38:31 +0900

    +11651

---
+++ algorithm-BOJ-step/src/BOJ_sort/BOJ11651.java
@@ -0,0 +1,38 @@
+package BOJ_sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ11651 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		int[][] cooridinates = new int[N][2];
+		
+		for (int i = 0; i < cooridinates.length; i++) {
+			st = new StringTokenizer(br.readLine()," ");			
+			cooridinates[i][0] =Integer.parseInt(st.nextToken());
+			cooridinates[i][1] =Integer.parseInt(st.nextToken());			
+		}
+		
+		Arrays.sort(cooridinates, (e1, e2) -> { 
+			if(e1[1] == e2[1]) {
+				return e1[0] - e2[0];
+			} else {
+				return e1[1] - e2[1];
+			}
+		});
+		
+		for (int i = 0; i < N; i++) {
+			System.out.println(cooridinates[i][0]+" "+cooridinates[i][1]);
+		}
+
+	}
+
+}
