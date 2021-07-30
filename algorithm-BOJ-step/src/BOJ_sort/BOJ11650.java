5063e43a1f814a7b6f15e8fbb77e7bdb8ed5cd0a
Minseok-Choi <cms05041@gmail.com>
2021-07-30 21:30:27 +0900

    +have to review comparable,comparator,lambda

--- algorithm-BOJ-step/src/BOJ_sort/BOJ11650.java
+++ algorithm-BOJ-step/src/BOJ_sort/BOJ11650.java
@@ -20,18 +20,20 @@ public class BOJ11650 {
 			cooridinates[i][0] =Integer.parseInt(st.nextToken());
 			cooridinates[i][1] =Integer.parseInt(st.nextToken());			
 		}
-		Arrays.sort(cooridinates, (e1, e2) -> {
+		
+		Arrays.sort(cooridinates, (e1, e2) -> { 
 			if(e1[0] == e2[0]) {
 				return e1[1] - e2[1];
 			} else {
 				return e1[0] - e2[0];
 			}
 		});
+		
 		for (int i = 0; i < N; i++) {
 			System.out.println(cooridinates[i][0]+" "+cooridinates[i][1]);
 		}
-
-
+//		https://st-lab.tistory.com/243
+//		https://st-lab.tistory.com/110?category=857114
 	}
 
 }
