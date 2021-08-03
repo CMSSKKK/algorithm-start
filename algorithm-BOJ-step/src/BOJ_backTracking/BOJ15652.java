b1191b29347ba8fff3803a6bedd82044d709d1de
Minseok-Choi <cms05041@gmail.com>
2021-08-03 20:08:22 +0900

    + N and M

--- algorithm-BOJ-step/src/BOJ_backTracking/BOJ15652.java
+++ algorithm-BOJ-step/src/BOJ_backTracking/BOJ15652.java
@@ -5,17 +5,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ15650 {
-	
-	public static int[] numbers;
-	public static int N, M;
+public class BOJ15652 {
+
+	public static int numbers[];
+	public static int N,M;
 	public static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		StringTokenizer st= new StringTokenizer(br.readLine()," ");
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
@@ -23,25 +22,21 @@ public class BOJ15650 {
 		numbers = new int[M];
 		dfs(1,0);
 		System.out.println(sb);
-	
+		
 	}
 	
 	public static void dfs(int start, int depth) {
 		if(depth == M) {
-			for (int num : numbers) {
+			for(int num : numbers) {
 				sb.append(num).append(" ");
-				
 			}
 			sb.append('\n');
 			return;
 		}
 		
 		for (int i = start; i <= N; i++) {
-			
 			numbers[depth] = i;
-			
-			dfs(i+1,depth+1);
-			
-		}			
+			dfs(i, depth+1);
+		}
 	}
 }
