70aa647336b2cddd30a296c30518f4da066a73f6
Minseok-Choi <cms05041@gmail.com>
2021-09-09 19:02:52 +0900

    add funcImp

---
+++ programmers-practice/src/programmers/functionImprove.java
@@ -0,0 +1,45 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+
+public class functionImprove {
+
+	public static void main(String[] args) {
+		
+		int[] progresses = {93,30,55};
+		int[] speeds = {1,30,5};
+		
+		 Queue<Integer> queue = new LinkedList<>();
+	     List<Integer> answerList = new ArrayList<>();
+
+	        for (int i = 0; i < speeds.length; i++) {
+	            double remain = (100 - progresses[i]) / (double) speeds[i];
+	            int date = (int) Math.ceil(remain);
+
+	            if (!queue.isEmpty() && queue.peek() < date) {
+	                answerList.add(queue.size());
+	                queue.clear();
+	            }
+
+	            queue.offer(date);  
+	        }
+
+	        answerList.add(queue.size());
+
+	        int[] answer = new int[answerList.size()];
+
+	        for (int i = 0; i < answer.length; i++) {
+	            answer[i] = answerList.get(i);
+	            System.out.println(answer[i]);
+	        }
+
+	        
+	}
+		
+}
+
+
