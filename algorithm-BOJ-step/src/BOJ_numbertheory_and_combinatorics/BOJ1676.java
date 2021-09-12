dddb50e5065868173f02713ba2dbbc270774a213
Minseok-Choi <cms05041@gmail.com>
2021-09-12 18:14:19 +0900

    end numberTheory

---
+++ algorithm-BOJ-step/src/BOJ_numbertheory_and_combinatorics/BOJ1676.java
@@ -0,0 +1,21 @@
+package BOJ_numbertheory_and_combinatorics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1676 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int count = 0;
+		while(N >= 5) {
+			count += N/5;
+			N /= 5;
+		}
+		System.out.println(count);
+	}
+
+}
---
+++ algorithm-BOJ-step/src/BOJ_numbertheory_and_combinatorics/BOJ2004.java
@@ -0,0 +1,41 @@
+package BOJ_numbertheory_and_combinatorics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2004 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int twoResult = two(N)-two(N-M)-two(M);
+		int fiveResult = five(N)-five(N-M)-five(M);
+		System.out.println(Math.min(twoResult, fiveResult));
+	}
+	
+	public static int two(int num) {
+		int count = 0;
+		while(num>=2) {
+			count += num/2;
+			num /= 2;
+		}
+		return count;
+	}
+	
+	public static int five(int num) {
+		int count = 0;
+		while(num>=5) {
+			count += num/5;
+			num /= 5; 
+		}
+		
+		return count;
+	}
+
+}
