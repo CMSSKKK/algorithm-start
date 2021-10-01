34d0654d88dca465e47cbb3327b2e5b08a8d32cd
Minseok-Choi <cms05041@gmail.com>
2021-10-01 09:19:49 +0900

    add 1920

--- algorithm-BOJ-step/src/BOJ_binary_search/BOJ1920.java
+++ algorithm-BOJ-step/src/BOJ_binary_search/BOJ1920.java
@@ -7,11 +7,12 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ1920 {
-
+		public static int[] numN;
+		
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[] numN = new int[N];
+		numN = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		for (int i = 0; i < numN.length; i++) {
 			numN[i] = Integer.parseInt(st.nextToken());
@@ -22,20 +23,37 @@ public class BOJ1920 {
 		int M = Integer.parseInt(br.readLine());
 		
 		st = new StringTokenizer(br.readLine()," ");
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < M; i++) {
 			
-			if(bs(Integer.parseInt(st.nextToken()))) {
-				
+			if(bs(Integer.parseInt(st.nextToken())) >= 0) {
+				sb.append(1).append('\n');
 			} else {
-				
+				sb.append(0).append('\n');
 			}
 		}
 		
-		
+		System.out.println(sb);
 	}
 	
-	public static boolean bs(int val) {
-		return false;
+	public static int bs(int val) {
+		int start = 0;
+		int last = numN.length-1;
+		
+		while(start <= last) {
+			
+			int mid = (start+last)/2;
+			
+			if(val < numN[mid]) {
+				last = mid-1;
+			} else if(val > numN[mid]) {
+				start = mid+1;
+			} else {
+				return mid;
+			}
+		}
+		
+		return -1;
 	}
 
 }
