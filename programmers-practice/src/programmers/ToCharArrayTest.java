4ad12bac537c1c093f4684d04adf34a822f472fe
Minseok-Choi <cms05041@gmail.com>
2021-10-13 23:40:18 +0900

    add 14916

---
+++ programmers-practice/src/programmers/ToCharArrayTest.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class ToCharArrayTest {
+
+	public static void main(String[] args) {
+		String str = "abcd";
+		int answer = 0;
+		answer  = str.length();
+		char[] arr = str.toCharArray();
+ 		System.out.println(answer);
+		String ans = "";
+		ans += arr[1];
+		System.out.println(ans);
+	}
+
+}
